089b0f22c1fe913930b7fccee6e69561fb9d9300
Aram Zadikian <brancusi@gmail.com>
2017-03-22 20:48:47 -0700

    add logging

--- index.js
+++ index.js
@@ -98,10 +98,14 @@ function startApp() {
       peripheral.discoverServices(["fffffffffffffffffffffffffffffff0"], function(err, services){
         console.log("Discover services");
         const service = services.filter(service => service.uuid === "fffffffffffffffffffffffffffffff0")[0];
-        const chars = service.discoverCharacteristics([], (err, characteristics) => {
-          console.log("Setting instance");
-          inst = characteristics.filter(char => char.uuid === "fffffffffffffffffffffffffffffff4")[0];
-        });
+
+        if(service) {
+
+          const chars = service.discoverCharacteristics([], (err, characteristics) => {
+            console.log("Setting instance");
+            inst = characteristics.filter(char => char.uuid === "fffffffffffffffffffffffffffffff4")[0];
+          });
+        }
 
       })
     });
